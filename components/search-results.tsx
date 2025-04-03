ce26e44803d4903f623de3ec72fa1d68181e4278
AgnetaA <agnetaaxelsson@hotmail.se>
2025-04-03 16:39:28 +0200

    Search function done

---
+++ components/search-results.tsx
@@ -0,0 +1,17 @@
+
+
+interface searchResultsProps {
+    query: string | null;
+}
+
+export default function SearchResults({ query }: searchResultsProps) {
+
+
+
+
+    return (
+        <section>
+            
+        </section>
+    )
+}
