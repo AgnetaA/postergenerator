d3c1ee6bfcacf5b75726986d1fa8e1c6b3d542d4
AgnetaA <agnetaaxelsson@hotmail.se>
2025-04-03 16:06:29 +0200

    useRouter

--- components/search-function.tsx
+++ components/search-function.tsx
@@ -2,35 +2,32 @@
 
 import styles from "./search.module.css";
 import { useState } from "react";
-
-// import React from "react";
-
+import { useRouter } from "next/navigation";
 
 
-export default function SearchFunction () {
 
-  const[searchQuery, setSearchQuery] = useState<string>('');
+export default function SearchFunction() {
+
+  const router = useRouter();
+
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   const handleKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    event.preventDefault();
+  
     if (event.key === "Enter" && searchQuery.length > 0) {
-      console.log("damn");
+      router.push(`/search?query=${searchQuery}`);
     }
     else if (event.key === "Enter") {
       alert("Please don't send an empty search");
+    }
   }
-  }
 
+  return (
 
-    return(
-        
-        <form className={styles.quoteSearchForm}>
-          <label>Search quotes: </label>
-          <input type="text" id="search" name="search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeydown} />
+    <form className={styles.quoteSearchForm}>
+      <label>Search quotes: </label>
+      <input type="text" id="search" name="search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeydown} />
 
-
-
-          {/* value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeydown} */}
-        </form>
-    )
+    </form>
+  )
 }
