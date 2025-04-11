8315eb6eeafa799ea88137732b8aa64bb0014432
AgnetaA <agnetaaxelsson@hotmail.se>
2025-04-11 12:03:07 +0200

    page exists

---
+++ app/allquotes/page.tsx
@@ -0,0 +1,21 @@
+
+import { Quotes } from "@/interfaces";
+import styles from "./page.module.css";
+
+import Link from "next/link";
+// import { useSearchParams } from "next/navigation";
+import GetAllQuotes from "@/components/fetch-all-quotes";
+
+
+export default async function AllQuotes() {
+    
+
+    return (
+        <main id="main">
+            <h2>All quotes</h2>
+
+            <GetAllQuotes />
+
+        </main>
+    )
+}
---
+++ components/fetch-all-quotes.tsx
@@ -0,0 +1,76 @@
+"use client"
+import { Quotes } from "@/interfaces";
+import styles from "../app/allquotes/page.module.css";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+
+
+export default function GetAllQuotes() {
+    const searchParams = useSearchParams(); 
+    let param = searchParams.get("skip");
+
+    
+
+    let skip: number = 0;
+    let next: number = 0;
+
+    if (!param) {
+        skip = 0;
+    }
+    else {
+        skip = parseInt(param);
+        next = skip + 10;
+    }
+
+    console.log("skip, next:", skip, next);
+
+  
+
+    const [allquotes, setQuotes] = useState<Quotes[]>([]);
+
+    useEffect(() => {
+        const fetchQuotes = async () => {
+            try {
+                const response = await fetch(`https://dummyjson.com/quotes?limit=10&skip=${skip}`);
+                const data = await response.json();
+
+                const allquotes: Quotes[] = data.quotes;
+
+                console.log(allquotes);
+
+                setQuotes(allquotes || []);
+
+
+            }
+            catch (error) {
+                console.error("Error fetching quotes: ", error);
+            }
+        };
+        fetchQuotes();
+    }, []);
+
+    // skip = skip + 10;
+
+    return (
+       
+        <section className={styles.quotesContainer}>
+            {allquotes.length > 0 ? allquotes.map((quotes, i) => (
+
+                <article className={styles.quoteCard} key={i}>
+                    <p className={styles.quote}>{quotes.quote}</p>
+                    <p className={styles.author}>~{quotes.author}</p>
+                    
+
+                </article>
+
+            )) : <p>There are no more quotes</p>}
+
+            
+
+            <Link className={styles.idLink} href={`/allquotes?skip=${next}`}>➜</Link>
+
+       </section>
+    )
+}
